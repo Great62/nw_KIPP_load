80d2b4129c97abefe568288057f9d0fe5220930b
Adam <Adamkindel1234@gmail.com>
2023-11-08 12:10:53 +0100

    fix(): hostname in query body

--- nw_KIPP_load.js
+++ nw_KIPP_load.js
@@ -1,7 +1,16 @@
 async function init() {
   try {
     const response = await fetch(
-      `https://us-central1-sales-chatbot-f1521.cloudfunctions.net/fetchKIPPLoadData`
+      `https://us-central1-sales-chatbot-f1521.cloudfunctions.net/fetchKIPPLoadData`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          hostname: window.location.hostname,
+        }),
+      }
     );
     let data = await response.json();
   
