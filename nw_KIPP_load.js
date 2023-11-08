226a6b96635dcf71f3f6956c48bb54a06c87d361
Adam <Adamkindel1234@gmail.com>
2023-11-08 12:01:46 +0100

    feat(): dev ready

---
+++ nw_KIPP_load.js
@@ -0,0 +1,44 @@
+async function init() {
+  try {
+    const response = await fetch(
+      `https://us-central1-sales-chatbot-f1521.cloudfunctions.net/fetchKIPPLoadData`
+    );
+    let data = await response.json();
+  
+    const scriptURL = data.cdnLink;
+  
+    if (document.readyState === "loading") {
+      // The DOM is not yet fully loaded
+      document.addEventListener("DOMContentLoaded", () => {
+        // update the window object with the bot data
+        window.nwKIPPData = {
+          clientId: data.clientId,
+          shopProvider: data.shopProvider,
+          colours: data.colours,
+          faq: data.faq,
+          contactBtnUrl: data.contactBtnUrl,
+        }
+
+        insertScript(scriptURL);
+        document.removeEventListener("DOMContentLoaded", insertScript);
+      });
+    } else {
+      // The DOM is already fully loaded
+      insertScript(scriptURL);
+    }
+    
+    console.log('KIPP loader data: ', `inserted script ${scriptURL}`);
+  } catch (error) {
+    console.log('KIPP loader error: ', error);
+  }
+}
+
+init();
+
+const insertScript = (src) => {
+  const script = document.createElement("script");
+  script.src = src;
+  script.type = "module";
+  script.defer = true;
+  document.body.appendChild(script);
+}
